d1c73917a5567943bb453bf92106abda6d6756c3
MrMelonking <ravlik009@gmail.com>
2024-04-11 08:43:54 +0300

    Task1 done

---
+++ src/Lb1/task1/Task1.java
@@ -0,0 +1,40 @@
+package Lb1.task1;
+
+
+public class Task1 {
+    public static void main(String[] args) {
+
+
+        int number1 = 3;
+        int number2 = 6;
+        float number3 = 5.1f;
+        float number4 = 10.4f;
+
+        int sumInt = number1 + number2;
+
+        int substrInt = number2 - number1;
+
+        int multiInt = number1 * number2;
+
+        int diviInt = number2 / number1;
+
+        float sumFloat = number3 + number4;
+
+        float substrFloat = number4 - number3;
+
+        float multiFloat = number3 * number4;
+
+        float diviFloat = number4 / number3;
+
+        System.out.println(sumInt);
+        System.out.println(substrInt);
+        System.out.println(multiInt);
+        System.out.println(diviInt);
+        System.out.println(sumFloat);
+        System.out.println(substrFloat);
+        System.out.println(multiFloat);
+        System.out.println(diviFloat);
+    }
+
+
+}
